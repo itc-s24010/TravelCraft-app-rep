c55cb1afc197b1535971016a512067abd1d21e20
itc-s24010 <s24010@std.it-college.ac.jp>
2026-08-13 09:43:32 +0900

    renderデプロイエラー修正

--- artifacts/spring-api/src/main/java/com/travel/api/TravelApiApplication.java
+++ artifacts/spring-api/src/main/java/com/travel/api/TravelApiApplication.java
@@ -2,8 +2,9 @@ package com.travel.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class TravelApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(TravelApiApplication.class, args);
